c3e847ed1a0597463ae0c6ab075326c4f16209e7
WenHsi <wenxi0423@gmail.com>
2024-02-21 10:37:41 +0800

    modify cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,9 +1,9 @@
 import argparse
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Your CLI description here')
+    parser = argparse.ArgumentParser(description='LESS-video-sling')
     parser.add_argument('videopath', type=str, help='Path to the input video file')
-    parser.add_argument('label', type=str, help='Label for the type of processing (F or S)')
+    parser.add_argument('label', type=str, help='Label for the type of processing (F for front and S for side)')
     parser.add_argument('folderpath', type=str, help='Path to the folder for saving output files')
     return parser.parse_args()
 
