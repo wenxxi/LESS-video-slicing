564d93d04b2bac5eb84235a71f5c415215153bd0
WenHsi <wenxi0423@gmail.com>
2024-02-21 11:00:37 +0800

    merge cli.py into run.py

--- src/cli.py
+++ src/cli.py
@@ -3,7 +3,7 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser(description='LESS-video-sling')
     parser.add_argument('videopath', type=str, help='Path to the input video file')
-    parser.add_argument('label', type=str, help='Label for the type of processing (F for front and S for side)')
+    parser.add_argument('label', type=str, choices=['F', 'S'], help='Label for the type of processing (F for front and S for side)')
     parser.add_argument('folderpath', type=str, help='Path to the folder for saving output files')
     return parser.parse_args()
 
--- src/run.py
+++ src/run.py
@@ -1,35 +1,78 @@
 import cv2
 from moviepy.editor import VideoFileClip
 from utils import *
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='LESS-video-sling')
+    parser.add_argument('videopath', type=str, help='Path to the input video file')
+    parser.add_argument('label', type=str, choices=['F', 'S'], help='Label for the type of processing (F for front and S for side)')
+    parser.add_argument('folderpath', type=str, help='Path to the folder for saving output files')
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    videopath = args.videopath
+    label = args.label
+    folderpath = args.folderpath
+    txtpath = folderpath + 'log.txt'
+
+    cap = cv2.VideoCapture(videopath) 
+    clip = VideoFileClip(videopath)
+    width = cap.get(3)
+    height = cap.get(4)
+    TotalFrame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    if label == 'F':
+        framelist, RAnklePosY, LAnklePosY = front_view(cap, height, TotalFrame)
+        RAm_Y, LAm_Y, Am_framelist = smoothing(framelist, RAnklePosY, LAnklePosY)
+        Ldy_list, Rdy_list = calculate_slope(RAm_Y, LAm_Y, Am_framelist)
+        Kslice_frame = front_takeoff(Rdy_list, Ldy_list, Am_framelist)
+    elif label == 'S':
+        framelist, RAnklePosX, LAnklePosX = side_view(cap, height, TotalFrame)
+        RAm_X, LAm_X, Am_framelist = smoothing(framelist, RAnklePosX, LAnklePosX)
+        Ldx_list, Rdx_list = calculate_slope(RAm_X, LAm_X, Am_framelist)
+        Kslice_frame = side_takeoff(RAm_X, Rdx_list, LAm_X, Ldx_list, Am_framelist)
+
+    frame_txt(txtpath, Kslice_frame)
+    slice(clip, Kslice_frame, TotalFrame, folderpath)
+
+    cap.release()
+    clip.close()
+
+if __name__ == "__main__":
+    main()
+
 
 # input導入
-videopath = ''
-label = 'F'
-folderpath = ''
-txtpath = folderpath + 'log.txt'
+# videopath = ''
+# label = 'F'
+# folderpath = ''
+# txtpath = folderpath + 'log.txt'
 
-cap = cv2.VideoCapture(videopath) 
-clip = VideoFileClip(videopath)
-width =cap.get(3)
-height =cap.get(4)
-TotalFrame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+# cap = cv2.VideoCapture(videopath) 
+# clip = VideoFileClip(videopath)
+# width =cap.get(3)
+# height =cap.get(4)
+# TotalFrame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
-if label == 'F':
-    framelist, RAnklePosY, LAnklePosY = front_view(cap, height, TotalFrame)
-    RAm_Y, LAm_Y, Am_framelist = smoothing(framelist, RAnklePosY, LAnklePosY)
-    Ldy_list, Rdy_list = calculate_slope(RAm_Y, LAm_Y, Am_framelist)
-    Kslice_frame = front_takeoff(Rdy_list, Ldy_list, Am_framelist)
-elif label == 'S':
-    framelist, RAnklePosX, LAnklePosX = side_view(cap, height, TotalFrame)
-    RAm_X, LAm_X, Am_framelist = smoothing(framelist, RAnklePosY, LAnklePosY)
-    Ldx_list, Rdx_list = calculate_slope(RAm_Y, LAm_Y, Am_framelist)
-    Kslice_frame = side_takeoff(RAm_X, Rdx_list, LAm_X, Ldx_list, Am_framelist)
+# if label == 'F':
+#     framelist, RAnklePosY, LAnklePosY = front_view(cap, height, TotalFrame)
+#     RAm_Y, LAm_Y, Am_framelist = smoothing(framelist, RAnklePosY, LAnklePosY)
+#     Ldy_list, Rdy_list = calculate_slope(RAm_Y, LAm_Y, Am_framelist)
+#     Kslice_frame = front_takeoff(Rdy_list, Ldy_list, Am_framelist)
+# elif label == 'S':
+#     framelist, RAnklePosX, LAnklePosX = side_view(cap, height, TotalFrame)
+#     RAm_X, LAm_X, Am_framelist = smoothing(framelist, RAnklePosY, LAnklePosY)
+#     Ldx_list, Rdx_list = calculate_slope(RAm_Y, LAm_Y, Am_framelist)
+#     Kslice_frame = side_takeoff(RAm_X, Rdx_list, LAm_X, Ldx_list, Am_framelist)
 
-frame_txt(txtpath, Kslice_frame)
-slice(clip, Kslice_frame, TotalFrame, folderpath)
+# frame_txt(txtpath, Kslice_frame)
+# slice(clip, Kslice_frame, TotalFrame, folderpath)
 
-cap.release()
-clip.close()
+# cap.release()
+# clip.close()
 
 
 
